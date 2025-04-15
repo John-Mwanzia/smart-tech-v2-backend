6c1e64cd302ad85cbbaf70b76e70131a638b3920
John Mwanzia <johnmwanzia277@gmail.com>
2025-04-15 08:42:05 +0300

    feat: edit products repo

--- internals/handlers/products.go
+++ internals/handlers/products.go
@@ -1,9 +1,58 @@
 package handlers
 
-//products
+import (
+  "net/http"
+  "strings"
+)
+
+func ProductsHandler(w http.ResponseWriter, r *http.Request){
+  path := strings.Trim(r.URL.Path, "/")
+
+  switch r.Method {
+  case http.MethodGet:
+    switch path{
+    case "" :
+      getProducts(w,r)
+    case "/search":
+      searchProducts(w,r)
+    case "/categories":
+      getCategories(w, r)
+    case "/slug/:slug":
+      getProductBySlug(w,r)
+    case "id" :
+      getProductById(w,r)
+
+    } 
+  }
+
+}
 
 
-//featured products
+func getProducts(w http.ResponseWriter, r *http.Request){
+
+}
+
+
+
+func searchProducts(w http.ResponseWriter, r *http.Request){
+}
+
+
+func getCategories(w http.ResponseWriter, r *http.Request){
+}
+
+
+
+func getProductBySlug(w http.ResponseWriter, r *http.Request){
+}
+
+
+
+func getProductById(w http.ResponseWriter, r *http.Request){
+}
+
+
+
 
 
 
--- internals/repository/products_repo.go
+++ internals/repository/products_repo.go
@@ -1,1 +1,43 @@
 package repository
+
+import (
+  "context"
+  "fmt"
+  "time"
+
+  "github.com/John-Mwanzia/smart-tech-v2-backend/internals/config"
+  "github.com/John-Mwanzia/smart-tech-v2-backend/internals/models"
+  "go.mongodb.org/mongo-driver/bson"
+)
+
+func FetchProducts() (*[]models.Product, error) {
+  var products []models.Product
+  ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+  defer cancel()
+
+  cursor, err := config.DbInstance.ProductsCollection.Find(ctx, bson.M{})
+
+  if err != nil {
+    return nil, fmt.Errorf("failed to fetch products") 
+  }
+
+  for cursor.Next(ctx){
+    var product models.Product
+
+    if err := cursor.Decode(&product); err != nil {
+      return nil, fmt.Errorf("failed to decode products")
+    }
+
+    products = append(products, product)
+  }
+
+
+  return &products,nil 
+
+
+
+}
+
+
+
+
